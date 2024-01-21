566951968b7ec844bc77e14104adcf62b579cfc0
Ashkree <maveron.tyriel@gmail.com>
2024-01-21 23:31:03 +0400

    added graphics to snake and fruits

--- sneck.py
+++ sneck.py
@@ -27,13 +27,14 @@ class FRUIT:
 
     def __init__(self):
         self.randomize()
+        self.apple = pygame.image.load('graphics/apple.png').convert_alpha()
 
     def draw_fruit(self, screen):
 
         fruit_rect = pygame.Rect(
             int(self.pos.x * CELL_SIZE), int(self.pos.y * CELL_SIZE), CELL_SIZE, CELL_SIZE)
 
-        pygame.draw.rect(screen, TOMATO, fruit_rect)
+        screen.blit(self.apple, fruit_rect)
 
     def randomize(self):
         self.x = self.y = random.randint(0, CELL_NUMBER - 1)
@@ -52,15 +53,48 @@ class SNAKE:
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = self.RIGHT
         self.new_block = False
+        self.load_graphics()
 
     def draw_snake(self, screen):
 
-        for block in self.body:
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        for index, block in enumerate(self.body):
+
+            x_pos = int(block.x * CELL_SIZE)
+            y_pos = int(block.y * CELL_SIZE)
 
             block_rect = pygame.Rect(
-                int(block.x * CELL_SIZE), int(block.y * CELL_SIZE), CELL_SIZE, CELL_SIZE)
+                x_pos, y_pos, CELL_SIZE, CELL_SIZE)
 
-            pygame.draw.rect(screen, CORNFLOWER, block_rect)
+            if index == 0:
+
+                screen.blit(self.head, block_rect)
+
+            elif index == len(self.body) - 1:
+
+                screen.blit(self.tail, block_rect)
+
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                else:
+                    if (previous_block.x == -1 and next_block.y == -1) or (previous_block.y == -1 and next_block.x == -1):
+                        screen.blit(self.body_tl, block_rect)
+
+                    elif (previous_block.x == -1 and next_block.y == 1) or (previous_block.y == 1 and next_block.x == -1):
+                        screen.blit(self.body_bl, block_rect)
+
+                    elif (previous_block.x == 1 and next_block.y == -1) or (previous_block.y == -1 and next_block.x == 1):
+                        screen.blit(self.body_tr, block_rect)
+                    else:
+                        screen.blit(self.body_br, block_rect)
 
     def move_snake(self):
 
@@ -75,6 +109,49 @@ class SNAKE:
 
     def add_block(self):
         self.new_block = True
+
+    def load_graphics(self):
+
+        GRAPHICS = ["head_up", "head_down", "head_right", "head_left",
+                    "tail_up", "tail_down", "tail_right", "tail_left",
+                    "body_tr", "body_tl", "body_br", "body_bl",
+                    "body_horizontal", "body_vertical"]
+
+        for i in range(len(GRAPHICS)):
+
+            name = GRAPHICS[i]
+            path = f"graphics/body/{name}.png"
+
+            setattr(self, name, pygame.image.load(
+                path).convert_alpha())
+
+    def update_head_graphics(self):
+
+        # relative position of the head in relation to the first piece of the body
+        head_position = self.body[0] - self.body[1]
+
+        if head_position == self.RIGHT:
+            self.head = self.head_right
+        elif head_position == self.LEFT:
+            self.head = self.head_left
+        elif head_position == self.UP:
+            self.head = self.head_up
+        else:
+            self.head = self.head_down
+
+    def update_tail_graphics(self):
+
+        # relative position of the tail in relation to the body
+        tail_position = self.body[-1] - self.body[-2]
+
+        if tail_position == self.RIGHT:
+            self.tail = self.tail_right
+        elif tail_position == self.LEFT:
+            self.tail = self.tail_left
+        elif tail_position == self.UP:
+            self.tail = self.tail_up
+        else:
+            self.tail = self.tail_down
 
 
 class MAIN:
@@ -130,7 +207,7 @@ def main():
 
     # event timer, handles when to redraw the snake
     SCREEN_UPDATE = pygame.USEREVENT
-    pygame.time.set_timer(SCREEN_UPDATE, 150)
+    pygame.time.set_timer(SCREEN_UPDATE, 100)
 
     while True:
 
